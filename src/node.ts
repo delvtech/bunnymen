9c4bf9308d9d9b45d893d84e27971e997cd66907
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2023-01-14 12:09:13 -0600

    create nodeExample.ts (#10)
    * create nodeExample.ts
    
    * example runs now

--- src/node.ts
+++ src/node.ts
@@ -19,7 +19,7 @@ import path from 'path'
 import { nanoid } from 'nanoid'
 
 
-interface INodeEvents {
+export interface INodeEvents {
     subscribed: (property: string) => void
     unsubscribed: (property: string) => void
     peerSubscribed:(property: string) => void
@@ -76,7 +76,7 @@ export class Node extends EventEmitter {
         this.emit('subscribed', this._topic)
     }
 
-    async unSubscribe(topic: string) {
+    async unsubscribe(topic: string) {
         const node: IPFS.IPFS = await this._node
         node.pubsub.unsubscribe(topic)
         this.emit('unsubscribed', this._topic)
@@ -187,46 +187,3 @@ export class Node extends EventEmitter {
     }
 
 }
-
-const topic = '2134dfdfvsfbdf'
-const node = new Node(topic)
-
-let numPeers = 0
-let counter = 0
-let cid = ''
-
-node.on('subscribed', (peerId) => console.log('subscribed ' + peerId))
-node.on('unsubscribed', (peerId) => console.log('unsubscribed ' + peerId))
-node.on('peerSubscribed', (peerId) => console.log('peer subscribed ' + peerId))
-node.on('peerUnsubscribed', (peerId) => console.log('peer Unsubscribed ' + peerId))
-node.on('sentMessage', (message) => {
-    //console.log('sent message: ' + message)
-})
-node.on('receivedMessage', (message) => {
-    cid = message
-    //console.log('recieved message: ' + message)
-    node.download(cid)
-})
-node.on('uploadedData', (cid) => {
-    console.log("uploaded data: " + cid)
-    setInterval(async () => {
-        node.sendMessage(cid)
-    }, 2000)
-    
-})
-node.on('downloadedData', (data) => {
-    if( +data >= counter ){
-        counter = +data
-        console.log("downloaded data: " + data)
-        counter++
-        node.upload(counter.toString())
-    }
-})
-
-node.subscribe()
-node.poll(100)
-
-node.upload(counter.toString())
-
-
-
