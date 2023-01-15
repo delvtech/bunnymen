442c1c8a93bb4ba3becd24540e3f05e39cde28d3
William Villanueva <will@Williams-MacBook-Pro-2.local>
2023-01-15 19:21:14 +0100

    Updated terminal appllication and dependencies further

--- apps/chat-injector/src/main.ts
+++ apps/chat-injector/src/main.ts
@@ -2,4 +2,57 @@ import { Node } from 'bunnymen'
 
 // initiate node here
 // switch to a simple inline script
-console.log(Node);
+async function main() {
+    const topic = 'frank'
+    const node = new Node(topic)
+    
+    let numPeers = 0
+    let counter = 0
+    let cid = ''
+    
+    node.on('subscribed', (peerId) => console.log('subscribed ' + peerId))
+    node.on('unsubscribed', (peerId) => console.log('unsubscribed ' + peerId))
+    node.on('peerSubscribed', (peerId) => {
+        // only if there is a cid to send 
+        if(cid.length > 0 && node.isLeader()) {
+            // send latest cid when we see that a node has joined the channel
+            node.sendMessage(cid)
+        } else {
+            console.log('no data has been uploaded yet so there is no message to send')
+        }
+        console.log('peer subscribed ' + peerId)
+    })
+    node.on('peerUnsubscribed', (peerId) => console.log('peer unsubscribed ' + peerId))
+    node.on('sentMessage', (message) => {
+        //console.log('sent message: ' + message)
+    })
+    node.on('receivedMessage', (message) => {
+        cid = message
+        //console.log('recieved message: ' + message)
+        node.download(cid)
+    })
+    node.on('uploadedData', (newCid) => {
+        console.log("uploaded data: " + newCid)
+        cid = newCid
+        node.sendMessage(newCid)
+        
+    })
+    node.on('downloadedData', (data) => {
+        if( +data >= counter ){
+            counter = +data
+            console.log("downloaded data: " + data)
+            counter++
+            node.upload(counter.toString())
+        }
+    })
+    node.on('selectedLeader', (peerId) => {
+        console.log("new leader selected: " + peerId)
+    })
+    
+    node.subscribe()
+    node.poll(5000) 
+    node.upload(counter.toString())
+    
+}
+
+main()
--- packages/bunnymen-core/src/node.ts
+++ packages/bunnymen-core/src/node.ts
@@ -1,25 +1,23 @@
 import * as IPFS from 'ipfs-core'
 import { createLibp2p } from 'libp2p'
 import { MulticastDNS } from '@libp2p/mdns'
-// import { KadDHT } from '@libp2p/kad-dht'
-import { WebSockets } from '@libp2p/websockets'
-import { WebRTCStar } from '@libp2p/webrtc-star'
-import { Bootstrap } from '@libp2p/bootstrap'
-import { Mplex } from '@libp2p/mplex'
-import { Noise } from '@chainsafe/libp2p-noise'
+import { KadDHT } from '@libp2p/kad-dht'
+import { webSockets } from '@libp2p/websockets'
+import { webRTCStar } from '@libp2p/webrtc-star'
+import { bootstrap } from '@libp2p/bootstrap'
+import { mplex } from '@libp2p/mplex'
+import { noise } from '@chainsafe/libp2p-noise'
 import { GossipSub } from '@chainsafe/libp2p-gossipsub'
 import { PubSubPeerDiscovery } from '@libp2p/pubsub-peer-discovery'
-import { TCP } from '@libp2p/tcp'
+import type { CID } from 'multiformats/cid'
+import { PeerId, RSAPeerId } from '@libp2p/interface-peer-id'
+import { Message } from '@libp2p/kad-dht/dist/src/message'
 import { EventEmitter } from 'events'
 import os from 'os'
 import path from 'path'
 import { nanoid } from 'nanoid'
-import sha3 from 'js-sha3'
-
-// this package was installed as a dependency of ipfs-core, but isn't in the
-// package.json because if the versions get out of sync, the typescript server
-// will throw errors.
-import type { Message } from '@libp2p/interface-pubsub'
+import { createHash } from 'node:crypto'
+import { PeerIdStr } from '@chainsafe/libp2p-gossipsub/dist/src/types'
 
 
 export interface INodeEvents {
@@ -38,10 +36,10 @@ export class Node extends EventEmitter {
 
     private _topic: string
     private _node
-    private _peerId?: IPFS.Libp2pFactoryFnArgs['peerId']
+    private _peerId: PeerIdStr
     private _peers: string[]
-    private _currentLeader?: string
-    private _currentCid?: string
+    private _currentLeader: string = ''
+    private _currentCid: string = ''
     private _currentStep:  number = 0
     private _frequency = -1
     private POLLING_FREQENCY: number = 100
@@ -53,13 +51,11 @@ export class Node extends EventEmitter {
     constructor(topic: string) {
         super()
 
-        const libp2pBundle = (opts: IPFS.Libp2pFactoryFnArgs) => {
-
-            this._peerId = opts.peerId
-            const bootstrapList = opts.config.Bootstrap
-            const webRTCStar = new WebRTCStar()
+        const libp2pBundle = (opts) => {
+            const bootstrapList = opts.config.bootstrap
+            const wRTCStar = webRTCStar()
         
-            return this.configureLibp2p(this._peerId, webRTCStar, bootstrapList)
+            return this.configureLibp2p(this._peerId, bootstrapList)
         }
 
         this._node =  IPFS.create({
@@ -87,9 +83,8 @@ export class Node extends EventEmitter {
 
     async subscribe(): Promise<void> {
         const node: IPFS.IPFS = await this._node
-        node.pubsub.subscribe(this._topic, (message) => {
-            this.emit('receivedMessage', new TextDecoder().decode(message.data))
-        })
+        const receivedMessage = (message) => this.emit('receivedMessage', String.fromCharCode.apply(null, message.data))
+        node.pubsub.subscribe(this._topic,receivedMessage)
         this.emit('subscribed', this._topic)
         this.selectLeader()
     }
@@ -139,20 +134,18 @@ export class Node extends EventEmitter {
     }
 
     isLeader(){
-        return !!this._peerId && this._currentLeader == this._peerId.toString()
+        return this._currentLeader == this._peerId.toString()
     }
 
     private selectLeader(){
         var peers = this._peers
         // add local peerId to peer list
-        if (this._peerId) {
-            peers.push(this._peerId.toString())
-        }
+        peers.push(this._peerId.toString())
         // create a list of objects { peerId, hash }
         // where we hash each peer with the currentCid
         const peerHashList = peers.map(peer => {
             var preimage = peer+this._currentCid
-            return {peerId: peer, hash: sha3.sha3_256(preimage) }
+            return {peerId: peer, hash: createHash('sha3-256').update(preimage).digest('hex')}
         })
         // sort the list alphanumerically
         const peerHashListSorted = peerHashList.sort((a,b)=>{
@@ -184,59 +177,37 @@ export class Node extends EventEmitter {
       }
 
     // see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md
-    private configureLibp2p(peerId: any, webRTCStar: WebRTCStar, bootstrapList: any) {
-        console.log(bootstrapList)
+    private configureLibp2p(peerId: any, bootstrapList: any) {
+        const wRTCStar = webRTCStar()
         return createLibp2p({
-            peerId,
             addresses: {
-                listen: [
-                    '/ip4/0.0.0.0/tcp/0/ws',
-                    '/ip4/0.0.0.0/tcp/0',
-                    '/ip4/127.0.0.1/tcp/0/ws',
-                    '/ip4/127.0.0.1/tcp/0',
-                ]
-            },
-            connectionManager: {
-                pollInterval: 5000,
-                autoDial: true, // auto dial to peers we find when we dip below min peers
+              // Add the signaling server address, along with our PeerId to our multiaddrs list
+              // libp2p will automatically attempt to dial to the signaling server so that it can
+              // receive inbound connections from other peers
+              listen: [
+                '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+                '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star'
+              ]
             },
             transports: [
-                new WebSockets(),
-                new TCP(),
+              webSockets(),
+              wRTCStar.transport
             ],
-            streamMuxers: [
-                new Mplex()
-            ],
-            connectionEncryption: [
-                new Noise()
-            ],
+            connectionEncryption: [noise()],
+            streamMuxers: [mplex()],
             peerDiscovery: [
-                new MulticastDNS({
-                    interval: 10000
-                  }),
-                new Bootstrap({
-                    interval: 30e3,
-                    list: [bootstrapList]
-                }),
-                new PubSubPeerDiscovery({
-                    interval: 1000
-                })
-            ],
-            //dht: new KadDHT(),
-            // Turn on relay with hop active so we can connect to more peers
-            relay: {
-                enabled: true,
-                hop: {
-                    enabled: true,
-                    active: true
-                }
-            },
-            pubsub: new GossipSub({
-                enabled: true,
-                emitSelf: false,
-                allowPublishToZeroPeers: true,
-            })
-        })
+                wRTCStar.discovery,
+              bootstrap({
+                list: [
+                  '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+                  '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+                  '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
+                  '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+                  '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+                ]
+              })
+            ]
+          })
     }
 
 }
