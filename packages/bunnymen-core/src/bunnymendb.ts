23d3fa07e59ad6a399f54782abcd83b2dbe96d36
Ryan Goree <goree.ryan@gmail.com>
2023-01-15 17:13:28 -0600

    Add updated bunnyment from dev branch

--- packages/bunnymen-core/src/bunnymendb.ts
+++ packages/bunnymen-core/src/bunnymendb.ts
@@ -1,11 +1,13 @@
-import { IDataset } from './dataset'
-import { Transformer } from './types'
+import { IDataset } from './dataset.js'
+import { Transformer } from './types.js'
 
 export interface IBunnymenDB {
   registerDatasets: (key: string, ...dataSets: IDataset[]) => boolean
   registerTransformer: (key: string, transformer: Transformer) => boolean
   init: () => void
   get: <TData extends any = any>(key: string) => Promise<TData>
+  set: (key: string, data: any) => Promise<void>
+  subscribe: (key: string, handler: (data: any) => void) => void
 }
 
 export class BunnymenDB implements IBunnymenDB {
@@ -22,19 +24,19 @@ export class BunnymenDB implements IBunnymenDB {
     return true
   }
 
-  init() {
+  async init(): Promise<void> {
     const datasetArrays = Object.values(this.datasets)
     const allDatasets = ([] as IDataset[]).concat(...datasetArrays)
-    for (const dataset of allDatasets) {
-      dataset.init()
-    }
+    await Promise.all(allDatasets.map((dataset) => dataset.init()))
   }
 
   // TODO: how to add strong types to key arg and return value
   async get(key: string) {
+    console.log('BUNNYMEN: getting', key)
     const datasets = this.datasets[key]
 
     if (!datasets) {
+      // console.log('BUNNYMEN: no datasets for', key)
       return null
     }
 
@@ -45,16 +47,42 @@ export class BunnymenDB implements IBunnymenDB {
       rawDatas.push(rawData)
     }
 
+    // console.log('BUNNYMEN: rawDatas for', key, rawDatas)
+
     const transformer = this.transformers[key]
 
     if (transformer) {
+      // console.log('BUNNYMEN: transformer for', key, transformer)
       return transformer(...rawDatas)
     }
 
     if (rawDatas.length === 1) {
+      // console.log('BUNNYMEN: returning', rawDatas[0], 'for', key)
       return rawDatas[0]
     }
 
     return rawDatas
+  }
+
+  async set(key: string, data: any) {
+    const datasets = this.datasets[key]
+
+    for (const dataset of datasets) {
+      console.log('BUNNYMEN: setting', key, 'to', data)
+      await dataset.set(data)
+    }
+  }
+
+  subscribe(key: string, handler: (data: any) => void) {
+    const datasets = this.datasets[key]
+
+    // what happens if a dataset is registered after I've already subscribed?
+    if (!datasets) {
+      return false
+    }
+
+    for (const dataset of datasets) {
+      dataset.subscribe(handler)
+    }
   }
 }
