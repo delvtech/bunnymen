e7cf532dc1b5bddb827edb12e990f218b6fe5c28
Ryan Goree <goree.ryan@gmail.com>
2023-01-15 07:36:21 -0600

    Remove broken type

--- packages/bunnymen-core/src/types.ts
+++ packages/bunnymen-core/src/types.ts
@@ -1,18 +1,6 @@
-import { EventEmitter } from 'events'
-
 // shared types go here
 
 export type Transformer<
   TRawData extends any[] = any[],
   TData extends any = any
 > = (...rawData: TRawData) => TData
-
-export type IEvents = Record<string, (property: any) => void>
-
-export interface TypedEventEmitter<TEvents extends IEvents> {
-  on: <K extends keyof TEvents>(event: K, listener: TEvents[K]) => this
-  emit: <K extends keyof TEvents>(
-    event: K,
-    ...args: Parameters<TEvents[K]>
-  ) => boolean
-}
