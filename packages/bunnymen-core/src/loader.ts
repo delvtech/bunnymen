05d0a6c29537bda6273d7bf26a3ef62e0a84a576
Ryan Goree <goree.ryan@gmail.com>
2023-01-15 20:28:06 -0600

    Add transformer to loader

--- packages/bunnymen-core/src/loader.ts
+++ packages/bunnymen-core/src/loader.ts
@@ -9,14 +9,25 @@ export interface IPayload<TData> {
   lastUpdated: number
 }
 
-export interface ILoader<TData extends any = any> {
+export interface ILoaderOptions<TData = any, TNewData = TData> {
+  /**
+   * Used to transform new data coming in
+   */
+  transformer?: Transformer<[TNewData], TData>
+  /**
+   * Used to aggregate data instead of overwriting
+   */
+  aggregator?: Transformer<[TData | undefined, TData], TData>
+}
+
+export interface ILoader<TData = any, TNewData = TData> {
   /**
    * @returns CID
    */
   init: (node: Node, data: TData) => Promise<string>
   load: (
     node: Node,
-    newData: TData,
+    newData: TNewData,
     currentCID?: string
   ) => Promise<{
     cid: string
@@ -24,7 +35,7 @@ export interface ILoader<TData extends any = any> {
   }>
   loadHistorical: (
     node: Node,
-    oldData: TData,
+    oldData: TNewData,
     currentCID: string
   ) => Promise<{
     cid: string
@@ -33,34 +44,37 @@ export interface ILoader<TData extends any = any> {
   download: (node: Node, cid: string) => Promise<IPayload<TData>>
 }
 
-export class Loader<TData extends any = any>
-  implements ILoader<TData>
+export class Loader<TData = any, TNewData = TData>
+  implements ILoader<TData, TNewData>
 {
+  private transformer?: Transformer<[TData], TData>
   private aggregator?: Transformer<[TData | undefined, TData], TData>
 
-  constructor(
-    aggregator?: Transformer<[TData | undefined, TData], TData>,
-  ) {
-    this.aggregator = aggregator
+  constructor(options?: ILoaderOptions) {
+    this.transformer = options?.transformer
+    this.aggregator = options?.aggregator
   }
 
   /**
    * Factory method to return a strongly typed instance.
    */
-  static create<TData extends any = any>(
-    aggregator?: Transformer<[TData | undefined, TData], TData>,
-  ): Loader<TData> {
-    return new Loader(aggregator)
+  static create<TData = any, TNewData = any>(
+    options?: ILoaderOptions<TData, TNewData>
+  ): Loader<TData, TNewData> {
+    return new Loader(options)
   }
 
   init(node: Node, data: TData) {
     return node.upload(stringify(data))
   }
 
-  async load(node: Node, newData: TData, currentCID?: string) {
+  async load(node: Node, newData: TNewData, currentCID?: string) {
     const payload = {
       data: newData as unknown as TData,
       lastUpdated: Date.now(),
+    }
+    if (this.transformer) {
+      payload.data = this.transformer(payload.data)
     }
     if (this.aggregator) {
       let currentPayload: IPayload<TData> | undefined = undefined
@@ -68,22 +82,24 @@ export class Loader<TData extends any = any>
         const json = await node.download(currentCID)
         currentPayload = JSON.parse(json)
       }
-      payload.data = this.aggregator(currentPayload?.data, newData)
+      payload.data = this.aggregator(currentPayload?.data, payload.data)
     }
     const cid = await node.upload(stringify(payload))
     return { cid, payload }
   }
 
-  async loadHistorical(node: Node, oldData: TData, currentCID: string) {
+  async loadHistorical(node: Node, oldData: TNewData, currentCID: string) {
     const payload = {
-      data: oldData,
+      data: oldData as unknown as TData,
       lastUpdated: Date.now(),
+    }
+    if (this.transformer) {
+      payload.data = this.transformer(payload.data)
     }
     if (this.aggregator) {
       const currentJSON = await node.download(currentCID)
       const currentPayload = JSON.parse(currentJSON)
-      console.log
-      payload.data = this.aggregator(oldData, currentPayload.data)
+      payload.data = this.aggregator(payload.data, currentPayload.data)
     }
     const cid = await node.upload(stringify(payload))
     return {
