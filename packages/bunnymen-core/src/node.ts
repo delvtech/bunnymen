031301de5a17f40fc2117a6ce2d2fc18caf10569
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2023-01-19 21:28:33 -0600

    we can now run signalling server locally to test (#30)

--- packages/bunnymen-core/src/node.ts
+++ packages/bunnymen-core/src/node.ts
@@ -258,12 +258,13 @@ export class Node extends EventEmitter {
         // libp2p will automatically attempt to dial to the signaling server so that it can
         // receive inbound connections from other peers
         listen: [
-          '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
-          '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+          //'/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+          //'/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
           '/ip4/0.0.0.0/tcp/0/ws',
           '/ip4/0.0.0.0/tcp/0',
           '/ip4/127.0.0.1/tcp/0/ws',
           '/ip4/127.0.0.1/tcp/0',
+          '/ip4/127.0.0.1/tcp/13579/ws/p2p-webrtc-star/', // local webrtc-star server
         ],
       },
       connectionManager: {
